cc3f5a9534157ee54eddc6e7c7d2c2a4b4aec58c
Stephen Charlesworth <stephen.charlesworth@cookmedical.com>
2018-02-04 16:05:29 -0500

    minor changes to zip and deploy

--- lambda_deploy/zip_for_lambda.sh
+++ lambda_deploy/zip_for_lambda.sh
@@ -3,15 +3,15 @@ export PYTHON_VERSION='python3.6'
 export VIRTUALENV='venv_petbot'
 
 # Clean up
-#rm -fr $VIRTUALENV
+rm -fr $VIRTUALENV
 rm $ZIP_FILE
 
 # Setup fresh virtualenv and install requirements
 # note: be sure your virtualenv is created using Python 3.*
-#virtualenv $VIRTUALENV
-#source $VIRTUALENV/bin/activate
-#pip install -r ../requirements.txt
-#deactivate
+virtualenv $VIRTUALENV
+source $VIRTUALENV/bin/activate
+pip install -r ../requirements.txt
+deactivate
 
 # Zip dependencies from virtualenv, and main.py
 cd $VIRTUALENV/lib/$PYTHON_VERSION/site-packages/
